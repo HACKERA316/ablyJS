9aabbc0eac36cf2f7919d715c246c81ae147d16a
HACKERA316 <ademmurat1616@gmail.com>
2022-09-05 16:12:16 +0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # welcome to ablyJS
+
+
+# 
+ably-script-start     ="on"<br>
+ably-script-secure    ="on|off|secMAX"<br>
+ably-script-refresh   ="off|on"<br>
+ably-script-link-curl ="on|off|linkOFF"
--- ably.js
+++ ably.js
@@ -1,3 +1,66 @@
-function ablyJsControl(){
-    const ablyJs = "";
+
+
+const bodyControl = document.querySelector("html");
+
+const dataset = bodyControl.dataset;
+
+console.log(dataset)
+
+function refreshController(){
+
+    if (dataset.ablyScriptRefresh === "on"){
+        //function text make
+        console.log('on')
+        refreshEnable();
+    }
+    else if (dataset.ablyScriptRefresh === "off"){
+        //function text make
+        console.log('off')
+        refreshDisable();
+    }
+    else if (dataset.ablyScriptRefresh === "refreshFULL"){
+        //function text make
+        refreshFull();
+        console.log('full')
+    }
+    else{
+        console.log('null prompt tag');
+    }
+//functions
+
+//function refresh full
+    function refreshFull(){
+    }
+
+
+//function refresh on
+    function refreshEnable(){
+
+    }
+
+//function refresh off
+    function refreshDisable(){
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
